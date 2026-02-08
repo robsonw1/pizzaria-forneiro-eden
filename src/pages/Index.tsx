36a174582e9b22b3d1d61344a7af7d16273ef5c0
robsonw1 <robsonwilliam1008@gmail.com>
2026-02-08 19:48:48 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,39 +5,33 @@ import { CartDrawer } from '@/components/CartDrawer';
 import { CheckoutModal } from '@/components/CheckoutModal';
 import { Footer } from '@/components/Footer';
 import { CustomerLoginModal } from '@/components/CustomerLoginModal';
-import { CustomerDashboard } from '@/components/CustomerDashboard';
 import { useLoyaltyStore } from '@/store/useLoyaltyStore';
+import { useLoyaltyRealtimeSync } from '@/hooks/use-loyalty-realtime-sync';
 import { useState } from 'react';
 
 const Index = () => {
   const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
   const currentCustomer = useLoyaltyStore((s) => s.currentCustomer);
-  const logout = useLoyaltyStore((s) => s.logoutCustomer);
+
+  // Sincronizar dados de loyalty em tempo real
+  useLoyaltyRealtimeSync();
 
   return (
     <div className="min-h-screen bg-background flex flex-col">
       <Header onLoginClick={() => setIsLoginModalOpen(true)} />
-      
+
       <main className="flex-1">
-        {currentCustomer ? (
-          <div className="container mx-auto px-4 py-8">
-            <CustomerDashboard onLogout={async () => {
-              await logout();
-            }} />
-          </div>
-        ) : (
-          <ProductCatalog />
-        )}
+        <ProductCatalog />
       </main>
 
       {/* Footer */}
-      <Footer 
+      <Footer
         onLoginClick={() => setIsLoginModalOpen(true)}
         onAdminClick={() => {}}
       />
 
       {/* Modals & Drawers */}
-      <CustomerLoginModal 
+      <CustomerLoginModal
         isOpen={isLoginModalOpen}
         onClose={() => setIsLoginModalOpen(false)}
         onSuccess={() => setIsLoginModalOpen(false)}
